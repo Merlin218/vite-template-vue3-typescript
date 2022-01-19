44ac98d12e7af5f6919f5c04e10015af24e378bf
鸭脖yaboo <863176846@qq.com>
2022-01-19 20:04:50 +0800

    chore: 验证代码commit规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    'vue/setup-compiler-macros': true,
-  },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    'airbnb-base',
-    'plugin:prettier/recommended',
-    'prettier',
-  ],
-  parserOptions: {
-    ecmaVersion: 13,
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-  },
-  plugins: ['vue', '@typescript-eslint'],
-  rules: {
-    'prettier/prettier': 'error',
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		'vue/setup-compiler-macros': true,
+	},
+	extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'airbnb-base', 'plugin:prettier/recommended', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 13,
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
+	},
+	plugins: ['vue', '@typescript-eslint'],
+	rules: {
+		'prettier/prettier': 'error',
+	},
 };
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<p>验证commit</p>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
